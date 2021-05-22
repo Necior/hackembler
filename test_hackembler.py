aeaee6407c74c801b32ae8a19722d64acc6d64b7
Adrian Sadłocha <adrian@sadlocha.eu>
2021-05-22 19:34:00 +0200

    Reformat tests

--- test_hackembler.py
+++ test_hackembler.py
@@ -14,7 +14,17 @@ class TestCInstruction:
 class TestAInstruction:
     def test_gen_code(self):
         st = SymbolTable()
-        assert AInstruction(value_xor_label=0).gen_code(st) == "0000000000000000"
-        assert AInstruction(value_xor_label=1).gen_code(st) == "0000000000000001"
-        assert AInstruction(value_xor_label=255).gen_code(st) == "0000000011111111"
-        assert AInstruction(value_xor_label=256).gen_code(st) == "0000000100000000"
+        assert (
+            AInstruction(value_xor_label=0).gen_code(st) == "0000000000000000"
+        )
+        assert (
+            AInstruction(value_xor_label=1).gen_code(st) == "0000000000000001"
+        )
+        assert (
+            AInstruction(value_xor_label=255).gen_code(st)
+            == "0000000011111111"
+        )
+        assert (
+            AInstruction(value_xor_label=256).gen_code(st)
+            == "0000000100000000"
+        )
